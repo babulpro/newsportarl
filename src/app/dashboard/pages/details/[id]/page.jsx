26917fc5f4a739f270d3aa2317148fd0a0ade543
babulpro <sh8657706@gmail.com>
2025-02-25 22:25:28 +0600

    idd

--- src/app/dashboard/pages/details/[id]/page.jsx
+++ src/app/dashboard/pages/details/[id]/page.jsx
@@ -1,12 +1,30 @@
-"use client"
-import { useParams } from 'next/navigation';
+ 
+import Details from '@/app/lib/component/utilityCom/About';
 import React from 'react';
 
-const Page = ({params}) => {
-    const { id } = useParams();
+const getData = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/getData/newsDetails?id=${id}`);
+      
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      return data?.data; // Safely access data
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+      return null;
+    }
+  };
+  
+const Page = async({params}) => {
+    const  id  = (await params).id
+    let data = await getData(id)
     return (
         <div>
-            this is the details page. id: {id}
+            <Details data={data[0]}/>  
+          
         </div>
     );
 };
